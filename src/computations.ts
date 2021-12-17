5db756a07b00ce4ee810df7a9bb3f3898c2a1a6a
zzptichka <shkvorets@gmail.com>
2021-12-16 22:14:27 -0500

    fix qf formula

--- src/computations.ts
+++ src/computations.ts
@@ -2,8 +2,8 @@ export function calculate_divisor(match_value: number, sum_square: number) {
   return match_value / sum_square;
 }
 
-export function calculate_grant_estimated_match(square: number, divisor: number) {
-  return square * divisor;
+export function calculate_grant_estimated_match(sum_sqrt: number, sum_value: number, divisor: number) {
+  return (sum_sqrt * sum_sqrt - sum_value) * divisor;
 }
 
 export function calculate_funding_estimated_match(value: number, sum_sqrt: number, grant_estimate: number) {
